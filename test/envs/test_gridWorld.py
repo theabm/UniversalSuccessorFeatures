b0e3ee131c914b9f3e644d7429400769f524ae8b
abermeom <andres.bermeo-marinelli@inria.fr>
2023-02-20 17:47:32 +0100

    fixed failing tests

--- test/envs/test_gridWorld.py
+++ test/envs/test_gridWorld.py
@@ -6,7 +6,7 @@ my_env = env.GridWorld()
 def test_goal_weights(goal = (0,0)):
     my_env.reset(goal = goal) 
 
-    theoretical_goal_w = np.zeros(my_env.length_x*my_env.length_y)
+    theoretical_goal_w = np.zeros(my_env.rows*my_env.columns)
     theoretical_goal_w[0] = 1
 
     comp = theoretical_goal_w == my_env.get_goal_weights()
@@ -16,9 +16,9 @@ def test_goal_weights(goal = (0,0)):
 def test_state_features(state = (0,0)):
 
     my_env.reset(start_position= state)
-    theoretical_features = np.zeros(my_env.length_x*my_env.length_y)
+    theoretical_features = np.zeros(my_env.rows*my_env.columns)
     theoretical_features[0] = 1
 
-    comp = theoretical_features == my_env.get_state_features()
+    comp = theoretical_features == my_env.get_current_state_features()
 
     assert comp.all()
