41538f1a555272af34f3ead7edabe05dffbfc4e1
abermeom <andres.bermeo-marinelli@inria.fr>
2023-02-22 09:43:29 +0100

    version with working q learning unittest- dirty

--- test/agents/test_learning.py
+++ test/agents/test_learning.py
@@ -18,7 +18,7 @@ discount_factor = 0.5
 #Ground truth values for the following configuration (discount = 0.5)
 # o o o
 # o o o 
-# o o g
+# o o g         #UP  #DOWN #RIGHT #LEFT
 q_gt_g1_s1 = [0.0625,0.125,0.125,0.0625]
 q_gt_g1_s2 = [0.125,0.250,0.250,0.0625]
 q_gt_g1_s3 = [0.250,0.500,0.250,0.125]
@@ -27,9 +27,10 @@ q_gt_g1_s5 = [0.125,0.500,0.500,0.125]
 q_gt_g1_s6 = [0.250,1.000,0.500,0.250]
 q_gt_g1_s7 = [0.125,0.250,0.500,0.250]
 q_gt_g1_s8 = [0.250,0.500,1.000,0.250]
-q_gt_g1_s9 = [0.500,1.000,1.000,0.500] #goal state
+# q_gt_g1_s9 = [np.nan,np.nan,np.nan,np.nan] 
+# q_gt_g1_s9 = [0.500,1.000,1.000,0.500] #corresponding Q(goal state, actions)
 
-q_gt_g1_list = np.array([q_gt_g1_s1, q_gt_g1_s2, q_gt_g1_s3, q_gt_g1_s4, q_gt_g1_s5, q_gt_g1_s6, q_gt_g1_s7, q_gt_g1_s8, q_gt_g1_s9])
+q_gt_g1_list = np.array([q_gt_g1_s1, q_gt_g1_s2, q_gt_g1_s3, q_gt_g1_s4, q_gt_g1_s5, q_gt_g1_s6, q_gt_g1_s7, q_gt_g1_s8])
 
 #Ground truth values for the following configuration (discount = 0.5)
 # o o o
@@ -40,21 +41,21 @@ q_gt_g2_s2 = [0.125,0.250,0.0625,0.250]
 q_gt_g2_s3 = [0.0625,0.125,0.0625,0.125]
 q_gt_g2_s4 = [0.250,1.000,0.250,0.500]
 q_gt_g2_s5 = [0.125,0.500,0.125,0.500]
-q_gt_g2_s6 = [0.0625,0.250,0.125,0.250] #goal state
-q_gt_g2_s7 = [0.500,1.000,0.500,1.000]
+q_gt_g2_s6 = [0.0625,0.250,0.125,0.250] 
+#q_gt_g2_s7 = [np.nan,np.nan,np.nan,np.nan] 
+# q_gt_g2_s7 = [0.500,1.000,0.500,1.000]
 q_gt_g2_s8 = [0.250,0.500,0.250,1.000]
 q_gt_g2_s9 = [0.125,0.250,0.250,0.500]
 
-q_gt_g2_list = np.array([q_gt_g2_s1, q_gt_g2_s2, q_gt_g2_s3, q_gt_g2_s4, q_gt_g2_s5, q_gt_g2_s6, q_gt_g2_s7, q_gt_g2_s8, q_gt_g2_s9])
+q_gt_g2_list = np.array([q_gt_g2_s1, q_gt_g2_s2, q_gt_g2_s3, q_gt_g2_s4, q_gt_g2_s5, q_gt_g2_s6, q_gt_g2_s8, q_gt_g2_s9])
 
 Transition = namedtuple("Transition", ("state", "goal", "action", "reward", "next_state", "terminated", "truncated"))
 
 def test_training(
     num_episodes=50,
-    goal_1_position=goal_1_position,
-    goal_2_position=goal_2_position,
-    env = my_test_env,
-    agent = mdqa.MultigoalDQNAgent(epsilon = 1.0, train_for_n_iterations = 1, discount_factor = discount_factor, network = {"cls": mdqn.StateGoalPaperDQN}) 
+    agent = mdqa.MultigoalDQNAgent(
+        epsilon = 1.0, train_for_n_iterations = 2, discount_factor = discount_factor, network = {"cls": mdqn.StateGoalPaperDQN}
+        ) 
 ):
     reward_per_episode = 0
     step = 0
@@ -85,7 +86,9 @@ def test_training(
             if terminated or truncated:
                 break
         
+    global goal_1_position
     goal_1_position = np.array(goal_1_position)
+    global goal_2_position
     goal_2_position = np.array(goal_2_position)
 
     q_pred_g1_list = []
@@ -93,15 +96,28 @@ def test_training(
     for i in range(my_test_env.rows):
         for j in range(my_test_env.columns):
             state = np.array([i,j])
-            q_pred_g1_list.append(agent.policy_net(**agent._make_compatible_with_nn(s = state, g = goal_1_position)).squeeze().cpu().detach().numpy())
-            q_pred_g2_list.append(agent.policy_net(**agent._make_compatible_with_nn(s = state, g = goal_2_position)).squeeze().cpu().detach().numpy())
+            if i*my_test_env.rows + j == 8:
+                q_pred_g2_list.append(agent.policy_net(**agent._make_compatible_with_nn(s = state, g = goal_2_position)).squeeze().cpu().detach().numpy())
+                continue
+            elif i*my_test_env.rows + j == 6:
+                q_pred_g1_list.append(agent.policy_net(**agent._make_compatible_with_nn(s = state, g = goal_1_position)).squeeze().cpu().detach().numpy())
+                continue
+            else:
+                q_pred_g1_list.append(agent.policy_net(**agent._make_compatible_with_nn(s = state, g = goal_1_position)).squeeze().cpu().detach().numpy())
+                q_pred_g2_list.append(agent.policy_net(**agent._make_compatible_with_nn(s = state, g = goal_2_position)).squeeze().cpu().detach().numpy())
 
     q_pred_g1_list = np.array(q_pred_g1_list)
     q_pred_g2_list = np.array(q_pred_g2_list)
-    
-    cmp1 = np.allclose(q_pred_g1_list, q_gt_g1_list, rtol = 0, atol = 0.1)
-    cmp2 = np.allclose(q_pred_g2_list, q_gt_g2_list, rtol = 0, atol = 0.1)
+
+    print("\n", q_gt_g1_list,"\n", q_pred_g1_list) 
+    print("\n", q_gt_g2_list,"\n", q_pred_g2_list) 
+    cmp1 = np.allclose(q_pred_g1_list, q_gt_g1_list, rtol = 0, atol = 0.01)
+    cmp2 = np.allclose(q_pred_g2_list, q_gt_g2_list, rtol = 0, atol = 0.01)
 
     assert cmp1 and cmp2
             
+    
+def build_q_table(goal_position, *args):
+    assert len(args) == 9
+
     
