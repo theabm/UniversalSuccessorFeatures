020ec7044ec52ad811e0fd9b3c2615e9105d8044
abermeom <andres.bermeo-marinelli@inria.fr>
2023-03-03 11:12:42 +0100

    added tests

--- test/memory/test_experienceReplayMemory.py
+++ test/memory/test_experienceReplayMemory.py
@@ -1,10 +1,16 @@
 import universalSuccessorFeatures.memory as mem
-from collections import namedtuple
+import numpy as np
 
 
 def test_build_push():
-    Transition = namedtuple("Transition", ("state,goal,reward"))
-    
     exp_repl = mem.ExperienceReplayMemory()
-    exp_repl.push(Transition(1,2,3))
-    assert len(exp_repl) == 1 and exp_repl[0].state == 1 and exp_repl[0].goal == 2 and exp_repl[0].reward == 3
+    exp_repl.push((1,2,3))
+    assert len(exp_repl) == 1 and exp_repl[0][0] == 1 and exp_repl[0][1] == 2 and exp_repl[0][2] == 3
+
+def test_sample(batch_size = 5):
+    exp_repl = mem.ExperienceReplayMemory()
+    for _ in range(batch_size*2):
+        exp_repl.push(np.random.rand(4))
+    
+    samples = exp_repl.sample(batch_size=batch_size)
+    assert len(samples) == batch_size
