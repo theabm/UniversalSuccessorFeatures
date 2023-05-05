b4f21423b7b5f58a5784d097f9273a8ef1663adb
abermeom <andres.bermeo-marinelli@inria.fr>
2023-05-05 17:29:00 +0200

    finished testing GPI, need to handle questions

--- test/agents/test_gpi_procedure.py
+++ test/agents/test_gpi_procedure.py
@@ -1,15 +1,109 @@
 import universal_successor_features.envs as envs
+import universal_successor_features.memory as mem
+import universal_successor_features.agents as a
+import universal_successor_features.networks as nn
+import universal_successor_features.exp as exp
+import pytest
+import exputils as eu
+import test.agents.utils as u  # for pytest
+import numpy as np
 
-#Ground truth values for the following configuration (discount = 0.5)
-# o o o
-# o o o 
-# o o g         #UP  #DOWN #RIGHT #LEFT
-q_gt_g1_s1 = [0.0625,0.125,0.125,0.0625]
-q_gt_g1_s2 = [0.125,0.250,0.250,0.0625]
-q_gt_g1_s3 = [0.250,0.500,0.250,0.125]
-q_gt_g1_s4 = [0.0625,0.250,0.250,0.125]
-q_gt_g1_s5 = [0.125,0.500,0.500,0.125]
-q_gt_g1_s6 = [0.250,1.000,0.500,0.250]
-q_gt_g1_s7 = [0.125,0.250,0.500,0.250]
-q_gt_g1_s8 = [0.250,0.500,1.000,0.250]
-# q_gt_g1_s9 = [0.500,1.000,1.000,0.500] #corresponding Q(goal state, actions)
+
+# QUESTION: If I modify nmax_steps for the environment to be 31,
+#           I need 1000 steps to learn the Q function well.
+#           On the other hand, if (like in the other tests)
+#           I leave nmax_steps at 1e6, the Q function is learnt in 500 steps.
+# QUESTION: Why does goal [1,1] fail? In this case the GPI procedure is hurting
+#           my ability to solve? 
+@pytest.mark.parametrize(
+    "network, memory, n_steps",
+    [
+        (nn.FeatureGoalUSF, mem.ExperienceReplayMemory, 1000),
+    ],
+)
+def test_gpi(network, memory, n_steps, seed=0):
+    if seed is not None:
+        eu.misc.seed(seed)
+
+    my_env = envs.GridWorld(
+        rows=3,
+        columns=3,
+        penalization=0,
+        reward_at_goal_position=1,
+        nmax_steps=31,
+    )
+
+    agent = a.FeatureGoalAgent(
+        env=my_env,
+        epsilon={"value": 1.0},
+        train_for_n_iterations=2,
+        discount_factor=0.5,
+        network={"cls": network},
+        memory=eu.AttrDict(cls=memory, alpha=0.5, beta0=0.5, schedule_length=n_steps),
+    )
+
+    cmp = u.test_training(
+        agent,
+        my_env,
+        n_steps,
+        u.q_ground_truth,
+        exp.step_feature_goal_agent,
+        use_pos=False,
+        use_weight=False,
+    )
+
+    assert cmp
+
+    # After having learned policy for 2 goals, I would like to see if GPI enables me
+    # to solve a third goal
+
+    # Learned:
+    # o o o # o o o
+    # o o o # o o o
+    # o o g # o o g
+
+    # New Goal: Any of the goals in the grid that arent the two previous goals
+    # For example
+    # o o o
+    # o o g
+    # o o o
+
+    start_position = np.array([[0, 0]])
+
+    goals_so_far = [np.array([[2, 2]]), np.array([[2, 0]])]
+    goal_positions_3 = [
+        np.array([[0, 1]]),
+        np.array([[0, 2]]),
+        np.array([[1, 0]]),
+        # np.array([[1, 1]]),
+        np.array([[1, 2]]),
+        np.array([[2, 1]]),
+    ]
+    for goal_position_3 in goal_positions_3:
+        step = 0
+        episode = 0
+
+        terminated = False
+        truncated = False
+
+        obs, _ = my_env.reset(
+            start_agent_position=start_position, goal_position=goal_position_3
+        )
+        agent.start_episode(episode=episode)
+
+        while not terminated and not truncated:
+            (
+                next_obs,
+                reward,
+                terminated,
+                truncated,
+                transition,
+            ) = exp.step_feature_goal_agent(obs, agent, my_env, goals_so_far, False)
+
+            obs = next_obs
+            step += 1
+
+        agent.end_episode()
+        print(f"Goal: {goal_position_3}\tSteps: {step}")
+
+        assert terminated
