43b388420e41473d37f6624aeafdc5421aec1fa6
abermeom <andres.bermeo-marinelli@inria.fr>
2023-06-12 18:16:53 +0200

    adding HER - broken

--- universal_successor_features/exp/core.py
+++ universal_successor_features/exp/core.py
@@ -5,7 +5,7 @@ import copy
 import universal_successor_features as usf
 from collections import namedtuple
 
-Transition = namedtuple(
+PartialTransition = namedtuple(
     "Experiences",
     (
         "agent_position",
@@ -34,7 +34,8 @@ def run_rl_first_phase(config=None, **kwargs):
             cls=None,
             network=eu.AttrDict(cls=None),
         ),
-        n_steps=np.inf,
+        n_steps=48000,
+        augment_data=False,
         log_name_step="step",
         log_name_episode="episode",
         log_name_step_per_episode="step_per_episode",
@@ -44,8 +45,8 @@ def run_rl_first_phase(config=None, **kwargs):
         log_name_total_reward="total_reward",
         log_name_done_rate_primary="done_rate_primary",
         log_name_done_rate_tertiary="done_rate_tertiary",
-        log_name_agent = "agent",
-        log_name_env = "env",
+        log_name_agent="agent",
+        log_name_env="env",
     )
 
     config = eu.combine_dicts(kwargs, config, default_config)
@@ -81,17 +82,28 @@ def run_rl_first_phase(config=None, **kwargs):
 
         goal_position = my_env.sample_source_goal()
 
+        # if I want to have data augmentation while I train, I augment over all
+        # the goals possible.
+        # Otherwise I simply keep my current goal.
+        if config.augment_data:
+            list_of_goal_positions_for_augmentation = my_env.goal_list_source_tasks
+        else:
+            list_of_goal_positions_for_augmentation = [goal_position]
+
         obs, _ = my_env.reset(goal_position=goal_position)
 
         while not terminated and not truncated and step <= config.n_steps:
-            # In the first part of the training I do not use GPI so goals_so_far is
-            # the single goal I am working with. If I was using GPI it would be a list
-            # of goals I have seen so far.
+            # In the first part of the training I do not use GPI so
+            # goals_for_gpi is the single goal I am working with. If I was
+            # using GPI it would be a list of goals I have seen so far.
             next_obs, reward, terminated, truncated, transition = general_step_function(
-                obs, agent, my_env, goals_so_far=[goal_position], training=True
+                obs, agent, my_env, goals_for_gpi=[goal_position], training=True
             )
 
-            agent.train(transition=transition)
+            agent.train(
+                transition=transition,
+                list_of_goal_positions_for_augmentation=list_of_goal_positions_for_augmentation,
+            )
 
             reward_per_episode += reward
             total_reward += reward
@@ -151,6 +163,7 @@ def run_rl_second_phase(config=None, **kwargs):
         use_gpi_eval=False,
         use_gpi_train=False,
         use_target_tasks=True,
+        augment_data=False,
         log_name_step="step",
         log_name_episode="episode",
         log_name_step_per_episode="step_per_episode",
@@ -161,8 +174,8 @@ def run_rl_second_phase(config=None, **kwargs):
         log_name_done_rate_source="done_rate_source",
         log_name_done_rate_eval="done_rate_eval",
         log_name_done_rate_combined="done_rate_combined",
-        log_name_agent = "agent",
-        log_name_env = "env",
+        log_name_agent="agent",
+        log_name_env="env",
     )
 
     config = eu.combine_dicts(kwargs, config, default_config)
@@ -237,20 +250,37 @@ def run_rl_second_phase(config=None, **kwargs):
         agent.start_episode(episode=episode)
 
         goal_position = goal_sampler()
-        goals_so_far = [goal_position]
+
+        # goals for gpi is only for action selection or evaluation
+        # it determines over which goals the gpi procedure should be used.
+        # if done during action selection, this will change the experiences I
+        # store in my memory buffer
+        goals_for_gpi = [goal_position]
+
+        # On the other hand, list_of_goal_positions_for_augmentation is for
+        # training directly
+        # It specifies the goals that I will use to augment my data.
+        if config.augment_data:
+            list_of_goal_positions_for_augmentation = (
+                goal_list_for_eval + my_env.goal_list_source_tasks
+            )
+        else:
+            list_of_goal_positions_for_augmentation = [goal_position]
 
         if config.use_gpi_train:
-            goals_so_far += my_env.goal_list_source_tasks
+            goals_for_gpi += my_env.goal_list_source_tasks
 
         obs, _ = my_env.reset(goal_position=goal_position)
 
         while not terminated and not truncated and step <= config.n_steps:
             next_obs, reward, terminated, truncated, transition = general_step_function(
-                obs, agent, my_env, goals_so_far, training=True
+                obs, agent, my_env, goals_for_gpi, training=True
             )
 
             agent.train_during_eval_phase(
-                transition=transition, p_pick_new_memory_buffer=0.5
+                transition=transition,
+                p_pick_new_memory_buffer=0.5,
+                list_of_goal_positions_for_augmentation=list_of_goal_positions_for_augmentation,
             )
 
             reward_per_episode += reward
@@ -307,21 +337,22 @@ def run_rl_second_phase(config=None, **kwargs):
     log.add_single_object("env", my_env)
     log.save()
 
+
 def evaluate_agent(agent, test_env, step_fn, goal_list_for_eval, use_gpi):
     num_goals = len(goal_list_for_eval)
     completed_goals = 0
 
     for goal in goal_list_for_eval:
-        goals_so_far = [goal]
+        goals_for_gpi = [goal]
         if use_gpi:
-            goals_so_far += test_env.goal_list_source_tasks
+            goals_for_gpi += test_env.goal_list_source_tasks
 
         terminated = False
         truncated = False
         obs, _ = test_env.reset(goal_position=goal)
         while not terminated and not truncated:
             next_obs, reward, terminated, truncated, _ = step_fn(
-                obs, agent, test_env, goals_so_far=goals_so_far, training=False
+                obs, agent, test_env, goals_for_gpi=goals_for_gpi, training=False
             )
             obs = next_obs
 
@@ -333,68 +364,65 @@ def evaluate_agent(agent, test_env, step_fn, goal_list_for_eval, use_gpi):
     return done_rate
 
 
-def general_step_function(obs, agent, my_env, goals_so_far, training):
-    """Choose an action and take a step in the environment. 
+def general_step_function(obs, agent, my_env, goals_for_gpi, training):
+    """Choose an action and take a step in the environment.
 
     Creates the transition and returns it among the various arguments.
     """
     action = agent.choose_action(
         obs=obs,
-        list_of_goal_positions=goals_so_far,
+        list_of_goal_positions=goals_for_gpi,
         training=training,
     )
 
     next_obs, reward, terminated, truncated, _ = my_env.step(action=action)
 
-    transition = Transition(
+    transition = PartialTransition(
         obs["agent_position"],
         obs["agent_position_features"],
-        obs["goal_position"],
-        obs["goal_weights"],
+        # obs["goal_position"],
+        # obs["goal_weights"],
         action,
-        reward,
+        # reward,
         next_obs["agent_position"],
         next_obs["agent_position_features"],
-        terminated,
-        truncated,
+        # terminated,
+        # truncated,
     )
 
     return next_obs, reward, terminated, truncated, transition
 
 
 if __name__ == "__main__":
-
     # Only for debugging, can delete later
     config = eu.AttrDict(
         # random seed for the repetition
-        seed = 3487 + 0,
-        env = eu.AttrDict(
-            cls = usf.envs.RoomGridWorld,
-            penalization = 0.0,
-            reward_at_goal_position = 20.0,
-            nmax_steps = 31,
+        seed=3487 + 0,
+        env=eu.AttrDict(
+            cls=usf.envs.RoomGridWorld,
+            penalization=0.0,
+            reward_at_goal_position=20.0,
+            nmax_steps=31,
         ),
         agent=eu.AttrDict(
-            cls = usf.agents.FeatureGoalAgent,
+            cls=usf.agents.FeatureGoalAgent,
             network=eu.AttrDict(
                 cls=usf.networks.FeatureGoalUSF,
             ),
-            loss_weight_q = 1.0,
+            loss_weight_q=1.0,
             loss_weight_psi=0.01,
-            loss_weight_phi = 0.0,
+            loss_weight_phi=0.0,
             discount_factor=0.90,
             batch_size=32,
             learning_rate=5e-4,
-            epsilon=eu.AttrDict(
-                value=0.25
+            epsilon=eu.AttrDict(value=0.25),
+            memory=eu.AttrDict(
+                cls=usf.memory.ExperienceReplayMemory,
+                alpha=None,
+                beta0=None,
             ),
-            memory = eu.AttrDict(
-                cls = usf.memory.ExperienceReplayMemory,
-                alpha = None,
-                beta0 = None,
-            )
         ),
-        n_steps = 48000,
+        n_steps=48000,
     )
 
     run_rl_first_phase(config=config)
