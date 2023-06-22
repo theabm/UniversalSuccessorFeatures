2010d21b4a4665b217b6784ded4b043558f48d58
abermeom <andres.bermeo-marinelli@inria.fr>
2023-06-22 17:53:32 +0200

    use optimal agents psi

--- universal_successor_features/exp/core.py
+++ universal_successor_features/exp/core.py
@@ -49,7 +49,7 @@ def run_rl_first_phase(config=None, **kwargs):
         log_name_total_reward="total_reward",
         log_name_done_rate_primary="done_rate_primary",
         log_name_done_rate_tertiary="done_rate_tertiary",
-        log_name_agent="agent",
+        log_name_agent="agent.pt",
         log_name_env="env",
     )
 
@@ -60,17 +60,102 @@ def run_rl_first_phase(config=None, **kwargs):
 
     if config.log_directory:
 
-        warnings.warn("Using pretrained agent")
+        warnings.warn("Using pretrained agent... loading env")
 
         # if config.log_directory is specified, then I will train my agent
         # using another pretrained agent as behavioral policy, i.e. to
         # select actions.
         # Since this agent was pretrained on an environment, we need to
         # use that same environment for the training of the current agent.
-        log.load(directory=config.log_directory, load_objects=True)
-        my_env = log.get_item(config.log_name_env)
+
+        # log.load(directory=config.log_directory, load_objects=True)
+        # my_env = log.get_item(config.log_name_env)
+
+        my_env = usf.envs.GridWorld.load_from_checkpoint(
+            config.log_directory + config.log_name_env
+        )
+
+        warnings.warn("Using pretrained agent... loading agent")
+
+        # if config.log_directory is specified, we will use a pretrained
+        # agent as a behavioral policy.
+
+        # note that the pretrained agent is supposed to be an "optimal" agent
+        # that has already learned to solve the task and will select optimal
+        # actions from the space.
+
+        # This is useful to understand whether our current agent can actually
+        # learn with the current architecture present.
+
+        # In our case, since we want to understand whether the agent can learn
+        # the true psi function (without no q loss), we first train an agent
+        # only with q loss (since we have seen that this is easy to do) and
+        # then use this agent as a behavioral policy that selects the actions
+        # the agent will observe and subsequently, train on.
+
+        # behavioral_agent = log.get_item(config.log_name_agent)
+
+        optimal_agent = usf.agents.BaseAgent.load_from_checkpoint(
+            my_env, config.log_directory + config.log_name_agent
+        )
+
+        # After having obtained the agent, we reset the log so that
+        # we dont have the data from the previous agent as well.
+
+        # log.reset()
+
+        # we assert that the saved goals of the agent and the environment 
+        # are the same (should be guaranteed but better safe than sorry)
+
+        assert all(
+            [
+                (goal1 == goal2).all()
+                for goal1, goal2 in zip(
+                    my_env.goal_list_source_tasks, optimal_agent._env_primary_goals
+                )
+            ]
+        ), "The agent did not learn the goals of this environment"
+
+        assert all(
+            [
+                (goal1 == goal2).all()
+                for goal1, goal2 in zip(
+                    my_env.goal_list_target_tasks, optimal_agent._env_secondary_goals
+                )
+            ]
+        ), "The agent did not learn the goals of this environment"
+
+        assert all(
+            [
+                (goal1 == goal2).all()
+                for goal1, goal2 in zip(
+                    my_env.goal_list_evaluation_tasks,
+                    optimal_agent._env_tertiary_goals,
+                )
+            ]
+        ), "The agent did not learn the goals of this environment"
+
+        # we set the training parameter for the general step function. This
+        # will get fed to agent.choose_action(). When we are using a pretrained
+        # agent, we dont want any exploration and want to get the optimal
+        # actions each time.
+        # Update: setting training = False and actually running the exp.
+        # results in an agent that is not able to learn. This is because the
+        # agent only sees a specific subset of the state,action space.
+        # Since the agent initially has random values for every (s,a)
+        # combination, this means that only for a subset of them, it learns
+        # the true value. However, the rest of (s,a) pairs are never updated
+        # and remain arbitrarily high/low. These values can lead the agent
+        # to select bad actions.
+        # a first solution is setting training = True
+        # this means the optimal agent will still do some exploration.
+        # Update: unsurprisingly, it doesnt work. so now we switch to using 
+        # the psi of the optimal agent as target but letting the agent still 
+        # do its own stuff.
+        config.agent.optimal_target_net = optimal_agent.target_net
 
     else:
+        warnings.warn("Using new agent... creating env")
         # otherwise, I create an environment from scratch.
         my_env = eu.misc.create_object_from_config(
             config.env,
@@ -82,40 +167,6 @@ def run_rl_first_phase(config=None, **kwargs):
 
     # the agent that will be trained during the episodes of the environment
     agent = eu.misc.create_object_from_config(config.agent, env=my_env)
-
-    if config.log_directory:
-        # if config.log_directory is specified, we will use a pretrained
-        # agent as a behavioral policy.
-        # note that the pretrained agent is supposed to be an "optimal" agent
-        # that has already learned to solve the task and will select optimal
-        # actions from the space.
-        # This is useful to understand whether our current agent can actually
-        # learn with the current architecture present.
-        # In our case, since we want to understand whether the agent can learn
-        # the true psi function (no q loss), we first train an agent only with
-        # q loss (since we have seen that this is easy to do) and then use this
-        # agent as a behavioral policy that selects the actions the agent will
-        # observe and subsequently, train on.
-        behavioral_agent = log.get_item(config.log_name_agent)
-
-        # After having obtained the agent, we reset the log so that
-        # we dont have the data from the previous agent as well.
-        log.reset()
-
-        # we set the training parameter for the general step function. This
-        # will get fed to agent.choose_action(). When we are using a pretrained
-        # agent, we dont want any exploration and want to get the optimal
-        # actions each time.
-
-        training = False
-    else:
-        # otherwise, the behavioral policy is simply the agent itself as
-        # it is learning what is the best thing to do.
-        behavioral_agent = agent
-
-        # if we dont have a behavioral agent, we will need some exploration.
-        # so we set training to be true.
-        training = True
 
     list_of_goal_positions_for_augmentation = my_env.goal_list_source_tasks
 
@@ -145,10 +196,10 @@ def run_rl_first_phase(config=None, **kwargs):
             # using GPI it would be a list of goals I have seen so far.
             next_obs, reward, terminated, truncated, transition = general_step_function(
                 obs,
-                behavioral_agent,
+                agent,
                 my_env,
                 goals_for_gpi=[goal_position],
-                training=training,
+                training=True,
             )
 
             agent.train(
@@ -193,9 +244,12 @@ def run_rl_first_phase(config=None, **kwargs):
         log.add_value(config.log_name_step_per_episode, step_per_episode)
         log.add_value(config.log_name_reward_per_episode, reward_per_episode)
 
-    log.add_single_object(config.log_name_agent, agent)
-    log.add_single_object(config.log_name_env, my_env)
+    # log.add_single_object(config.log_name_agent, agent)
+    # log.add_single_object(config.log_name_env, my_env)
     log.save()
+
+    my_env.save(config.log_name_env)
+    agent.save(config.log_name_agent, episode, step, total_reward)
 
 
 def run_rl_second_phase(config=None, **kwargs):
@@ -225,11 +279,18 @@ def run_rl_second_phase(config=None, **kwargs):
     if config.seed is not None:
         eu.misc.seed(config.seed)
 
-    log.load(directory=config.log_directory, load_objects=True)
+    # log.load(directory=config.log_directory, load_objects=True)
 
     # build instances
-    my_env = log.get_item("env")
-    agent = log.get_item("agent")
+    # my_env = log.get_item("env")
+    # agent = log.get_item("agent")
+
+    my_env = usf.GridWorld.load_from_checkpoint(
+        config.log_directory + config.log_name_env
+    )
+    agent = usf.agents.BaseAgent.load_from_checkpoint(
+        config.log_directory + config.log_name_agent
+    )
 
     agent_saved_source_goals = agent.env.goal_list_source_tasks
     agent_saved_target_goals = agent.env.goal_list_target_tasks
